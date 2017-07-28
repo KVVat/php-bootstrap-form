d5390f41d074b2507128ec0f1b3a5b9288ac8fec
moa-watanabe <ko-watanabe@premoa.co.jp>
2017-07-28 10:02:23 +0900

    Testing...

--- PFBC/Validation/AlphaNumeric.php
+++ PFBC/Validation/AlphaNumeric.php
@@ -1,6 +1,6 @@
 <?php
 class Validation_AlphaNumeric extends Validation_RegExp {
-	protected $message = "Error: %element% must be alphanumeric (contain only numbers, letters, underscores, and/or hyphens).";
+	protected $message = "エラー: %element% は半角の英数記号である必要があります (数字、英字、アンダースコアおよびハイフンが許されます)"
 
 	public function __construct($message = "") {
 		parent::__construct('/^[a-zA-Z0-9_\-\s\:\,\&]+$/', $message);
